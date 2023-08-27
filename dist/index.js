2779659cac996cb83d44d1732e316e4ff2cc7c34
Ben Siebert <hello@ben-siebert.de>
2023-08-27 18:23:02 +0200

    Update

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+/**
+ * api/src/index.ts
+ *
+ * Author: Ben Siebert <hello@ben-siebert.de>
+ * Copyright: Copyright (c) 2018-2023 Ben Siebert. All rights reserved.
+ * License: Project License
+ * Created At: 27.08.2023
+ *
+ */
+console.log("hello-world");
+//# sourceMappingURL=index.js.map
